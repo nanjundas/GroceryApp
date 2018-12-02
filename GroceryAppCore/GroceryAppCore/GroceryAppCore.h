8bf93cbd138dca9e213429c67252e2bc29d3be07
Nanjunda Swamy <nanjunda>
2018-12-02 19:11:06 +0800

    Added Core Project for Data Handling

---
+++ GroceryAppCore/GroceryAppCore/GroceryAppCore.h
@@ -0,0 +1,19 @@
+//
+//  GroceryAppCore.h
+//  GroceryAppCore
+//
+//  Created by Nanjundaswamy Sainath on 2/12/18.
+//  Copyright © 2018 Nanjunda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GroceryAppCore.
+FOUNDATION_EXPORT double GroceryAppCoreVersionNumber;
+
+//! Project version string for GroceryAppCore.
+FOUNDATION_EXPORT const unsigned char GroceryAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GroceryAppCore/PublicHeader.h>
+
+
